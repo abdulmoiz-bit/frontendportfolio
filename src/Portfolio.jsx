2a627e62bb463b829d72f64e273bc5a7c2c7be48
abdulmoiz-bit <moizr2497@gmail.com>
2023-02-08 01:22:49 +0500

    portfolio changes

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -9,6 +9,7 @@ const Portfolio = () => {
                     My Portfolio
                 </h1>
                 <div className='grid'>
+
                     <article className='testimonial one'>
                         <div className='testimonialtext'>
                             <h3 className='heading'> Ecommerce Store </h3>
@@ -18,25 +19,26 @@ const Portfolio = () => {
                                 a billing address form, a smooth slider, and a fully responsive site.
                             </p>
                             <div className='portfoliobtn'>
-                                <a target="_blank" href='https://reactjsecommercestore.vercel.app/'>
+                                <a target="_blank" rel="noreferrer" href='https://reactjsecommercestore.vercel.app/'>
                                     <button className='btn'> View Demo </button> &nbsp; &nbsp;
                                 </a>
-                                <a target="_blank" href='https://github.com/abdulmoiz-bit/reactjsecommercestore'>
+                                <a target="_blank" rel="noreferrer" href='https://github.com/abdulmoiz-bit/reactjsecommercestore'>
                                     <button className='btn'> Github </button>
                                 </a>
                             </div>
                         </div>
                     </article>
-                  
+
                     <article className='testimonial two'>
                         <div className='testimonialtext'>
-                            <h3 className='heading'> Todo </h3>
-                            <p> This is a basic todo app having create and delete function with local storage methods. </p>
+                            <h3 className='heading'> Admin Dashboard </h3>
+                            <p> The React Admin Dashboard has designed by Material UI
+                                contained beautiful icons, recharts, products and users list with images.</p>
                             <div className='portfoliobtn'>
-                                <a target="_blank" href='https://reactjstodoapp.vercel.app/'>
+                                <a target="_blank" rel="noreferrer" href='https://reactjsadmindashboard.vercel.app/'>
                                     <button className='btn'> View Demo </button> &nbsp; &nbsp;
                                 </a>
-                                <a target="_blank" href='https://github.com/abdulmoiz-bit/reactjstodoapp'>
+                                <a target="_blank" rel="noreferrer" href='https://github.com/abdulmoiz-bit/reactjsadmindashboard'>
                                     <button className='btn'> Github </button>
                                 </a>
                             </div>
@@ -45,14 +47,60 @@ const Portfolio = () => {
 
                     <article className='testimonial three'>
                         <div className='testimonialtext'>
+                            <h3 className='heading'> Calculator </h3>
+                            <p>Addition, subtraction, multiplication, and division actions in this calculator using the useReducer hook. </p>
+                            <div className='portfoliobtn'>
+                                <a target="_blank" rel="noreferrer" href='https://reactjscalculatorr.netlify.app/'>
+                                    <button className='btn'> View Demo </button> &nbsp; &nbsp;
+                                </a>
+                                <a target="_blank" rel="noreferrer" href='https://github.com/abdulmoiz-bit/reactjscalculatorr'>
+                                    <button className='btn'> Github </button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article className='testimonial four grid-col-span-2'>
+                        <div className='testimonialtext'>
+                            <h3 className='heading'> Todo </h3>
+                            <p> This is a basic todo app having create and delete function with local storage methods. </p>
+                            <div className='portfoliobtn'>
+                                <a target="_blank" rel="noreferrer" href='https://reactjstodoapp.vercel.app/'>
+                                    <button className='btn'> View Demo </button> &nbsp; &nbsp;
+                                </a>
+                                <a target="_blank" rel="noreferrer" href='https://github.com/abdulmoiz-bit/reactjstodoapp'>
+                                    <button className='btn'> Github </button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article className='testimonial five'>
+                        <div className='testimonialtext'>
                             <h3 className='heading'> TextUtils </h3>
                             <p> With this amazing TextUtils app, you can convert text into uppercase,
                                 lowercase copy text, word counters, and more with a dark mode theme.  </p>
                             <div className='portfoliobtn'>
-                                <a target="_blank" href='https://reactjstextutils.vercel.app/'>
+                                <a target="_blank" rel="noreferrer" href='https://reactjstextutils.vercel.app/'>
                                     <button className='btn'> View Demo </button> &nbsp; &nbsp;
                                 </a>
-                                <a target="_blank" href='https://github.com/abdulmoiz-bit/reactjstextutils'>
+                                <a target="_blank" rel="noreferrer" href='https://github.com/abdulmoiz-bit/reactjstextutils'>
+                                    <button className='btn'> Github </button>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+                    <article className='testimonial five'>
+                        <div className='testimonialtext'>
+                            <h3 className='heading'> Expense Tracker </h3>
+                            <p>Expense Tracker shows add income amount with a plus and expense amount with a minus.
+                                Delete previous amounts with the help of useReducer and ContextAPI.
+                            </p>
+                            <div className='portfoliobtn'>
+                                <a target="_blank" rel="noreferrer" href='https://reactjsexpensetracker.vercel.app/'>
+                                    <button className='btn'> View Demo </button> &nbsp; &nbsp;
+                                </a>
+                                <a target="_blank" rel="noreferrer" href='https://github.com/abdulmoiz-bit/reactjsexpensetracker'>
                                     <button className='btn'> Github </button>
                                 </a>
                             </div>
